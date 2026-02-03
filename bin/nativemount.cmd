92ef26c3a75dcb69f224c71c47cafaf0b9e790c6
anattagen <romjacket@protonmail.com>
2026-02-02 21:50:51 -0800

    update

--- bin/nativemount.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-REM nativemount.cmd.set - Native Windows Mount Template
-set "DISCIMAGE=%~1"
-if "%DISCIMAGE%"=="" goto :eof
-if exist "drvltr" del /q "drvltr"
-
-powershell -Command "$m = Mount-DiskImage -ImagePath '%DISCIMAGE%' -PassThru; if ($m) { ($m | Get-Volume).DriveLetter | Out-File -FilePath 'drvltr' -Encoding ASCII -NoNewline }"
--- clean.bat
+++ clean.bat
@@ -1,8 +1,16 @@
-for /f "delims=" %%a in ('dir /b/ad/s "*__pycache__"') do rmdir /s/q "%%~a"
-rmdir /s/q "Launchers"
-rmdir /s/q "Profiles"
-rmdir /s/q "build"
-del /q "Launcher.spec"
-del /q "*.log"
-del /q "rjpids.ini"
-del /q "steam.json"
+
+for /f "delims=" %%a in ('dir \b\ad\s "*__pycache__"') do rmdir \s\q "%%~a"
+rmdir \s\q "Launchers"
+rmdir \s\q "Profiles"
+rmdir \s\q "build"
+rmdir \s\q "dist"
+attrib +h bin\antimicrox
+attrib +h bin\wincdemu
+attrib +h bin\borderlessgaming
+attrib +h bin\multimonitortool
+attrib +h bin\imount
+del \q "*.log"
+del \q "bin\launcher.old"
+del \q "*.del"
+del \q "rjpids.ini"
+del \q "steam.json"
